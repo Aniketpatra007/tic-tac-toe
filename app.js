c139d62d19e3082af2fb24cf3732f1d876123532
Aniket Patra <iamaniketpatra@gmail.com>
2026-04-19 10:54:04 +0530

    Move Firebase config to gitignored config.js file

--- app.js
+++ app.js
@@ -7,16 +7,7 @@ import {
   onValue,
   onDisconnect,
 } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-database.js";
-
-const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT_ID-default-rtdb.firebaseio.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-};
+import { firebaseConfig } from "./config.js";
 
 const WIN_LINES = [
   [0, 1, 2],
